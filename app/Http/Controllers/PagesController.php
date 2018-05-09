46f4755a98058858725f0ccafd29bb073ab13f80
d890503 <2280259901@qq.com>
2018-05-09 09:57:48 +0000

    [add] Finish basic page layout

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 我们将使用控制器 PagesController 来处理所有自定义页面的逻辑，并使用 root() 方法来处理首页的展示。
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function root()
+    {
+        return view('pages.root');
+    }
+}
