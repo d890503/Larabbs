df3c479f8c7973a4f9ff33f72af576e46e1b733f
d890503 <2280259901@qq.com>
2018-05-14 05:47:44 +0000

    [add] Topic index page

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -45,6 +45,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Justin';
         $user->email = 'luckystar8953613@gmail.com';
+        $user->password = bcrypt('geomedia');
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
     }
