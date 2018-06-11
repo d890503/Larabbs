567c1420a9c5a109cbc0dab72ef1568705b3df2e
d890503 <2280259901@qq.com>
2018-06-11 03:38:52 +0000

    [add] List user replies

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //运行有现有顺序， 如果表需要另一个表的外键 ID ，  则优先执行外键 ID 依赖表
         $this->call(UsersTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
+        $this->call(ReplysTableSeeder::class);
     }
 }
